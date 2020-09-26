0d0d051781331a7c1b1fe2c724dc79cb32d26c18
DaJiangCK <dadack.mail@gmail.com>
2020-09-26 08:35:38 -0400

    .

--- app.py
+++ app.py
@@ -1,5 +1,48 @@
 from flask import Flask, request, jsonify
+import requests
+import time
+import smtplib
+from lxml.html import fromstring
+
 app = Flask(__name__)
+
+@app.route('/getitem/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    url = request.args.get("url", None)
+    size = request.args.get("sz", None)
+
+    # For debugging
+    print(f"got url {url}")
+    print(f"got fize {size}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not url or not size:
+        response["ERROR"] = "no url or size found, please send a name and size."
+    # Now the user entered a valid name
+    else:
+        to_email = 'mayanjun0110@gmail.com'
+        key_word = "Sold out online"
+        page = requests.get(url+'&sz='+size)
+        tree = fromstring(page.content)
+        item_name = tree.findtext('.//title')
+        if key_word not in page.text:
+            s = smtplib.SMTP('smtp.gmail.com', 587)
+            s.starttls()
+            s.login(os.environ['G_USERNAME'], os.environ['G_PASSWD'])
+            message = 'Subject: Your item {} is in store\n\n{}'.format(item_name, page.url)
+            s.sendmail("sender_email_id", to_email, message)
+            s.quit()
+            item_urls.remove(page.url)
+            response["MESSAGE"] = f"Item {item_name} is in store!"
+        else:
+            response["MESSAGE"] = f"Item {item_name} is sold out!!"
+        
+
+    # Return the response in json format
+    return jsonify(response)
 
 # A welcome message to test our server
 @app.route('/')
